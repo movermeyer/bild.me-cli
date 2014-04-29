780ae56effe86a11f4bde2e25c90846dcabf5972
Mozillazg <opensource.mozillazg@gmail.com>
2014-04-29 17:47:30 +0800

    add [-q|--quiet] argument for hide progress bar

--- bild/bild.py
+++ bild/bild.py
@@ -4,7 +4,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from argparse import ArgumentParser
-from threading import Thread
+from threading import Thread, active_count
 from time import sleep
 import sys
 
@@ -36,36 +36,48 @@ def upload(f):
         return {'status': 1, 'message': e}
 
 
+def output_result(result, list_all):
+    if result['status'] == 1:
+        sys.stderr.write(result['message'] + '\n')
+    if list_all:
+        print('\n\n'.join(result['result']))
+    else:
+        print(result['result'][5])
+
+
 class ProgressBar(object):
-    def __init__(self, list_all):
+    def __init__(self, list_all, show=True):
         self.progress = {}
         self.list_all = list_all
+        self.show = show
+
+    def output(self, name, n, size):
+        s = '{0}: [{1}]\r'.format(name, ('=' * n + '>').ljust(size))
+        sys.stdout.write(s)
+        sys.stdout.flush()
 
     def run(self):
-        f = sys.stdout
-
-        for (k, p) in self.progress.items():
-            for x in range(1, 50):
+        size = 50
+        for x in range(1, size + 1):
+            for (k, p) in self.progress.items():
+                _x = x
                 if p['finish']:
-                    x = 50
-                s = '{0}: [{1}]'.format(k, ('=' * x + '>').ljust(50, '-'))
-                f.write(s)
-                f.flush()
-                f.write('\r')
+                    _x = size
+                if self.show:
+                    self.output(k, _x, size)
 
-                if x != 50:
-                    sleep(0.3)
-                else:
-                    break
-            f.write('\n')
-            if p['finish']:
-                result = p['result']
-                if result['status'] == 1:
-                    sys.stderr.write(result['message'] + '\n')
-                if self.list_all:
-                    print('\n\n'.join(result['result']))
-                else:
-                    print(result['result'][5])
+                if p['finish']:
+                    if self.show:
+                        print('')
+                    result = p['result']
+                    output_result(result, self.list_all)
+                    del self.progress[k]
+                    continue
+
+                sleep(0.3)
+
+            if not self.progress:
+                break
 
 
 class UploadThread(Thread):
@@ -88,19 +100,25 @@ def main():
                         version=__version__)
     parser.add_argument('-l', '--list', action='store_true',
                         help='list all result')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help='decrease verbosity')
     parser.add_argument('-f', '-F', '--file', required=True,
                         nargs='+', help='picture file')
     args = parser.parse_args()
     files = set(args.file)
     list_all = args.list
-    bar = ProgressBar(list_all)
+    show = not args.quiet
+    bar = ProgressBar(list_all, show)
 
     for n, img in enumerate(files):
         t = UploadThread(img, bar)
         t.start()
 
-    sleep(0.2)
-    bar.run()
+    while True:
+        n = active_count()
+        if n == len(files):
+            bar.run()
+            break
 
 if __name__ == '__main__':
     main()
